6d419f39ad3cebd920ca8da595e591bc2869e76d
patric-dreier <patric.dreier@gmail.com>
2026-04-19 15:24:26 +0200

    Create linea.js
    Initial project structure

---
+++ src/linea.js
@@ -0,0 +1,19 @@
+/**
+ * LineaJS
+ * High-quality web typography for Webflow and beyond
+ * github.com/DEINUSERNAME/lineajs
+ */
+
+(function () {
+  'use strict';
+
+  const LineaJS = {
+    version: '0.1.0',
+    init: function () {
+      console.log('LineaJS ' + this.version + ' initialized');
+    }
+  };
+
+  window.LineaJS = LineaJS;
+  LineaJS.init();
+})();
